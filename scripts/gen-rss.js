7b3bb819ed439af714db33e4c2ae897414568262
Hitesh Taneja <hiteshtaneja307@gmail.com>
2024-01-19 05:43:37 +0530

    Basic footer changes

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Hitesh Taneja',
+    site_url: 'https://hiteshtaneja.com/',
+    feed_url: 'https://hiteshtaneja.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © Made with ❤️ <a href="https://www.linkedin.com/in/hiteshtanejaa/">Hitesh Taneja</a>.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
